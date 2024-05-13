911b05e009c16ddd029801e38653e7dac9f0a64c
Thijs Penning <t.j.penning@student.tudelft.nl>
2024-05-13 20:28:31 +0200

    Release workflow test

--- app/settings.py
+++ app/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
+print("=====================================")
+print(config('CONFIG_TEST'))
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
